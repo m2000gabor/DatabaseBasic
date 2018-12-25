00dd514023057f13c0962f2dec7a0624f8397896
m2000gabor <46127140+m2000gabor@users.noreply.github.com>
2018-12-25 18:56:50 +0100

    Initial commit

---
+++ app/src/main/java/com/example/android/databasebasic/NewRecordActivity.java
@@ -0,0 +1,97 @@
+package com.example.android.databasebasic;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class NewRecordActivity extends AppCompatActivity {
+
+    public static final String EXTRA_REPLY = "com.example.android.wordlistsql.REPLY";
+    private EditText mEditWordView;
+    private EditText mEditCatView;
+    private EditText mEditNumber;
+    boolean ForUpdate = false;
+    Intent replyIntent = new Intent();
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_record);
+        mEditWordView = findViewById(R.id.edit_word);
+        mEditCatView = findViewById(R.id.edit_cat);
+        mEditNumber = findViewById(R.id.edit_number);
+        Button button = findViewById(R.id.button_save);
+
+        ForUpdate = getIntent().getBooleanExtra("FOR_UPDATE",false);
+
+        //ha uj, a szoveget beallitani az editTextbe
+        if ( ForUpdate){
+            mEditWordView.setText(getIntent().getStringExtra("THE_TEXT"));
+            mEditCatView.setText(getIntent().getStringExtra("THE_CATEGORY"));
+            mEditNumber.setText(getIntent().getStringExtra("THE_NUMBER"));
+            button.setText(R.string.update_one_item);
+        }
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                //üres mező eseteén
+                if (TextUtils.isEmpty(mEditWordView.getText())) {
+                    setResult(RESULT_CANCELED, replyIntent);
+                }
+                //amikor nem ures
+                else {
+                    String word = mEditWordView.getText().toString();
+                    String cat = mEditCatView.getText().toString();
+                    int num = Integer.parseInt(mEditNumber.getText().toString());
+                    replyIntent.putExtra(EXTRA_REPLY, word);
+                    replyIntent.putExtra("EXTRA_CATEGORY", cat);
+                    replyIntent.putExtra("FOR_UPDATE",ForUpdate);
+                    replyIntent.putExtra("EXTRA_NUMBER",num);
+                    setResult(RESULT_OK, replyIntent);
+                }
+                finish();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+
+        if (!ForUpdate){getMenuInflater().inflate(R.menu.menu_new_record_activity, menu);}
+        else {getMenuInflater().inflate(R.menu.menu_new_record_activity, menu);}
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        switch (id){
+            case (R.id.action_delete) :
+                // custom action here
+                replyIntent.putExtra("FOR_DELETE",true);
+                ForUpdate=false;
+                replyIntent.putExtra("FOR_UPDATE",false);
+
+            case(R.id.action_cancel):
+                // custom action here
+                setResult(RESULT_CANCELED, replyIntent);
+                finish();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
